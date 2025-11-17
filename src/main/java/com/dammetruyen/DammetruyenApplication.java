16a258877e12153e6e638be6e7e0c17261dbef73
cuongphungquadeptrai <noname241103@gmail.com>
2025-11-17 22:28:32 +0700

    update

---
+++ src/main/java/com/dammetruyen/DammetruyenApplication.java
@@ -0,0 +1,14 @@
+package com.dammetruyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DammetruyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DammetruyenApplication.class, args);
+		System.out.println("http");
+	}
+
+}
